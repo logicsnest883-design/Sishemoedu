091a39e78f77901986fba5ffa1db083a3db4e34f
Joshua Kapilya <joshuakapilya15@gmail.com>
2026-09-09 13:12:26 +0200

    my commits

--- SchoolLibrary/settings.py
+++ SchoolLibrary/settings.py
@@ -87,11 +87,11 @@ import dj_database_url
 import dj_database_url
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        conn_max_age=600
+    )
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
